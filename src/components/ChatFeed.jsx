ab9b209d4a7b2cb32376f6514ea60457b6cd784e
Andrii Rastorhuiev <efinswim@gmail.com>
2022-08-27 19:03:19 +0300

    markup complete

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,12 +1,103 @@
-import React from 'react'
-import styles from './ChatFeed.module.scss'
+import React from 'react';
+import styles from './ChatFeed.module.scss';
+import sendIcon from '../assets/send.png';
 
 function ChatFeed() {
   return (
     <div className={styles.chatfeed}>
-      ChatFeed
+      <div className={styles.chatfeed__header}>
+        <img
+          src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
+          alt="Boris Johnson"
+        />
+        <p>Boris Johnson</p>
+      </div>
+      <div className={styles.chatfeed__content}>
+        <div className={styles.chatfeed__content_incoming}>
+          <div>
+            <img
+              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
+              alt="Boris Johnson"
+            />
+            <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+
+              <p>4/12/2022, 4:00 AM</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.chatfeed__content_sent}>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+          </div>
+          <p>4/12/2022, 4:00 AM</p>
+        </div>
+        <div className={styles.chatfeed__content_incoming}>
+          <div>
+            <img
+              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
+              alt="Boris Johnson"
+            />
+            <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+
+              <p>4/12/2022, 4:00 AM</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.chatfeed__content_sent}>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+          </div>
+          <p>4/12/2022, 4:00 AM</p>
+        </div>
+        <div className={styles.chatfeed__content_incoming}>
+          <div>
+            <img
+              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
+              alt="Boris Johnson"
+            />
+            <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+
+              <p>4/12/2022, 4:00 AM</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.chatfeed__content_sent}>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+          </div>
+          <p>4/12/2022, 4:00 AM</p>
+        </div>
+        <div className={styles.chatfeed__content_incoming}>
+          <div>
+            <img
+              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
+              alt="Boris Johnson"
+            />
+            <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+
+              <p>4/12/2022, 4:00 AM</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.chatfeed__content_sent}>
+          <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, quis.</p>
+          </div>
+          <p>4/12/2022, 4:00 AM</p>
+        </div>
+      </div>
+      <div className={styles.chatfeed__form}>
+        <span>
+          <input type="text" placeholder="Type your message" />
+          <img src={sendIcon} alt="send" />
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatFeed
+export default ChatFeed;
