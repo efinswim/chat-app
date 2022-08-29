77f7ef39f35483b4e7446f0185b56bd7681267e2
Andrii Rastorhuiev <efinswim@gmail.com>
2022-08-29 18:03:30 +0300

    connect api

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
 import styles from './ChatList.module.scss';
 
-import { useDispatch } from 'react-redux';
-import { fetchUsers, setUsers } from '../store/usersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store/usersSlice';
 
 function ChatList() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.userData.userList);
+  const status = useSelector((state) => state.userData.status);
+
+  console.log(users, 'user<<')
+
 
   useEffect(() => {
-    dispatch(fetchUsers)
-  }, [dispatch])
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   return (
     <div className={styles.chatlist}>
@@ -21,279 +26,20 @@ function ChatList() {
         <input type="search" placeholder="Search or start new chat" />
       </div>
       <div className={styles.chatlist__list}>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
+        {
+          users.map((user) => (
+            <div className={styles.chatlist__list_item}>
+              <div className={styles.chatlist__list_item_user}>
+                <img src={user.avatar} alt="Boris Johnson" />
+                <div>
+                  <p>{user.name}</p>
+                  <p>Slava Ukraine!!!!!!!!!</p>
+                </div>
+              </div>
+              <p>Jun 12, 2022</p>
             </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
-        <div className={styles.chatlist__list_item}>
-          <div className={styles.chatlist__list_item_user}>
-            <img
-              src="https://cdn.cnn.com/cnnnext/dam/assets/220412085815-boris-johnson-0407-super-tease.jpg"
-              alt="Boris Johnson"
-            />
-            <div>
-              <p>Boris Johnson</p>
-              <p>Slava Ukraine!!!!!!!!!</p>
-            </div>
-          </div>
-          <p>Jun 12, 2022</p>
-        </div>
+          ))
+        }
       </div>
     </div>
   );
--- src/store/usersSlice.js
+++ src/store/usersSlice.js
@@ -1,24 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  users: [],
+  userList: [],
   status: null,
   error: null,
 };
 
 export const fetchUsers = createAsyncThunk(
   'users/fetchUsers',
-  async function (_, { rejectWithValue, dispatch }) {
+  async function (_, { rejectWithValue }) {
     try {
       const response = await fetch('https://630cbb5f53a833c53433623a.mockapi.io/users');
-      console.log(response)
+
       if (!response.ok) {
         throw new Error('Server error');
       }
 
       const data = await response.json();
-
-      //dispatch(setUsers(data))
 
       return data;
     } catch (error) {
@@ -27,34 +25,26 @@ export const fetchUsers = createAsyncThunk(
   },
 );
 
-
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {
-    setUsers: (state, action) => {
-      state.users = action.payload
-    }
-  },
+  reducers: {},
   extraReducers: {
     [fetchUsers.pending]: (state, action) => {
       state.status = 'LOADING';
-      console.log(action.payload);
       state.error = null;
     },
     [fetchUsers.fulfilled]: (state, action) => {
       state.status = 'RESOLVED';
-      console.log(action.payload);
-      state.users = action.payload;
+      state.userList = action.payload;
     },
     [fetchUsers.rejected]: (state, action) => {
       state.status = 'REJECTED';
-      console.log(action.payload);
-      state.error = action.payload
-    }
+      state.error = action.payload;
+    },
   },
 });
 
-export const { setUsers } = usersSlice.actions
+export const { setUsers } = usersSlice.actions;
 
 export default usersSlice.reducer;
